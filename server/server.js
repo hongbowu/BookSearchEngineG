04d72e9e94ef1a4873c08ba0fe63cc0113c8f7ae
hongbowu <wuhongbo87@gmail.com>
2024-02-18 17:29:23 -0500

    use apollo server with schemas

--- server/server.js
+++ server/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
