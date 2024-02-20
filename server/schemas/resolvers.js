2de7a8519c8c4ec94931ab931772db4d10c3db01
hongbowu <wuhongbo87@gmail.com>
2024-02-20 17:28:23 -0500

    add query resolvers and login mutation

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,41 @@
+const { User, Book } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        user: async (parent, { userId }) => {
+            return User.findOne({ _id: userId });
+        },
+
+        me: async (parent, args, context) => {
+            if(context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw AuthenticationError;
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, { name, email, password }) => {
+            const user = await User.create({ name, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        }
+    }
+}
+module.exports = resolvers;
