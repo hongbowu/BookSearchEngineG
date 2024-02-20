dc4851d0300e7db2cfa1af5f499654611b0bbbae
hongbowu <wuhongbo87@gmail.com>
2024-02-20 18:15:25 -0500

    add typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,4 +12,25 @@ const typeDefs =`
         email: String!
         savedBooks: [Book]
     }
-`
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        user(userId: ID!): User
+        me: User
+    }
+
+    type Mutation {
+        addUser(name: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+
+        addBooksToUser(userId: ID!, savedBooks: [Book]): User
+        removeUser: User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
